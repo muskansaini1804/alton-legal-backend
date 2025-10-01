01ff24c9e353263ba3ab01050d8de06359e929d8
pankaj siwach <pankusiwach000@gmail.com>
2025-10-01 15:31:45 +0530

    add some more field of home section

--- types/generated/components.d.ts
+++ types/generated/components.d.ts
@@ -240,6 +240,8 @@ export interface PracticeAreaPracticeArea extends Struct.ComponentSchema {
   };
   attributes: {
     description: Schema.Attribute.String & Schema.Attribute.Required;
+    relatedCaseStudyText: Schema.Attribute.String & Schema.Attribute.Required;
+    relatedNewsText: Schema.Attribute.String & Schema.Attribute.Required;
     thumbnail: Schema.Attribute.Media<
       'images' | 'files' | 'videos' | 'audios'
     > &
